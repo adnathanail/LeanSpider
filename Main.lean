a6b0fd08665d0dddfcd8001a39aafef368ac6825
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-03-09 20:29:45 +0000

    Move more things to tests

--- Main.lean
+++ Main.lean
@@ -1,35 +1,4 @@
 import ZxLean
 
-def threeSpiders : ZXDiagram :=
-  .ofArrays #[.input 0, .spider .Z ⟨1, 2⟩, .spider .Z ⟨1, 1⟩, .spider .Z ⟨3, 4⟩, .output 0]
-            #[⟨0, 1⟩, ⟨1, 2⟩, ⟨2, 3⟩, ⟨3, 4⟩]
-
-#html threeSpiders.toHtml
-#html (threeSpiders.spiderFusion 2 3).get!.toHtml
-
-def zCnotZ : ZXDiagram :=
-  .ofArrays #[
-      .input 0, .spider .Z ⟨1, 1⟩, .spider .Z ⟨0, 1⟩, .spider .Z ⟨1, 1⟩, .output 0,
-      .input 1, .spider .X ⟨0, 1⟩, .output 1
-    ]
-    #[⟨0, 1⟩, ⟨1, 2⟩, ⟨2, 3⟩, ⟨3, 4⟩, ⟨2, 6⟩, ⟨5, 6⟩, ⟨6, 7⟩]
-
-def zCnotZFusedA := (zCnotZ.spiderFusion 1 2).get!
--- With stable IDs, after fusing 1+2, node 3 stays as 3 (not reindexed to 2)
-def zCnotZFusedB := (zCnotZFusedA.spiderFusion 1 3).get!
-
-#html zCnotZ.toHtml
-#html zCnotZFusedA.toHtml
-#html zCnotZFusedB.toHtml
-
-def useless_identity : ZXDiagram :=
-  .ofArrays #[.input 0, .spider .Z ⟨1, 1⟩, .spider .Z ⟨0, 1⟩, .output 0]
-            #[⟨0, 1⟩, ⟨1, 2⟩, ⟨2, 3⟩]
-
-def id_removed := (useless_identity.identityRemoval 1).get!
-
-#html useless_identity.toHtml
-#html id_removed.toHtml
-
 def main : IO Unit :=
   IO.println "Open Main.lean in VS Code to see the ZX diagram in the InfoView."
--- Tests.lean
+++ Tests.lean
@@ -38,4 +38,45 @@ def threeSpidersMerged2 : ZXDiagram :=  -- TODO simplify phase
 #lspec test "merging three spiders once" ((threeSpiders.spiderFusion 1 2).get! == threeSpidersMerged1) $
        test "merging three spiders twice" (((threeSpiders.spiderFusion 1 2).get!.spiderFusion 1 3).get! == threeSpidersMerged2)
 
+-- Spider fusing Z CNOT Z
+def zCnotZ : ZXDiagram :=
+  .ofArrays #[
+      .input 0, .spider .Z ⟨1, 1⟩, .spider .Z ⟨0, 1⟩, .spider .Z ⟨1, 1⟩, .output 0,
+      .input 1, .spider .X ⟨0, 1⟩, .output 1
+    ]
+    #[⟨0, 1⟩, ⟨1, 2⟩, ⟨2, 3⟩, ⟨3, 4⟩, ⟨2, 6⟩, ⟨5, 6⟩, ⟨6, 7⟩]
+#html zCnotZ.toHtml
+def cnot : ZXDiagram :=
+  { nodes := #[some (.input 0), some (.spider .Z ⟨2, 1⟩), none, none, some (.output 0), some (.input 1), some (.spider .X ⟨0, 1⟩), some (.output 1)]
+    edges := #[⟨0, 1⟩, ⟨5, 6⟩, ⟨6, 7⟩, ⟨1, 6⟩, ⟨1, 4⟩] }
+#html cnot.toHtml
+#lspec test "simplifying Z CNOT Z to just CNOT" ((((zCnotZ.spiderFusion 1 2).get!).spiderFusion 1 3).get! == cnot)
+
+-- Identity removal (Z spider)
+def justPhaseFreeZSpider : ZXDiagram :=
+  .ofArrays #[.input 0, .spider .Z ⟨0, 1⟩, .output 0]
+            #[⟨0, 1⟩, ⟨1, 2⟩]
+def justPhaseFreeZSpiderIdentityRemoved : ZXDiagram :=
+  { nodes := #[some (.input 0), none, some (.output 0)]
+    edges := #[⟨0, 2⟩] }
+
+#lspec test "removing phase-free Z spider" ((justPhaseFreeZSpider.identityRemoval 1).get! == justPhaseFreeZSpiderIdentityRemoved)
+
+-- Identity removal (X spider)
+def justPhaseFreeXSpider : ZXDiagram :=
+  .ofArrays #[.input 0, .spider .X ⟨0, 1⟩, .output 0]
+            #[⟨0, 1⟩, ⟨1, 2⟩]
+def justPhaseFreeXSpiderIdentityRemoved : ZXDiagram :=
+  { nodes := #[some (.input 0), none, some (.output 0)]
+    edges := #[⟨0, 2⟩] }
+
+#lspec test "removing phase-free X spider" ((justPhaseFreeXSpider.identityRemoval 1).get! == justPhaseFreeXSpiderIdentityRemoved)
+
+-- Identity removal error
+def aZSpiderWithPhase : ZXDiagram :=
+  .ofArrays #[.input 0, .spider .Z ⟨1, 1⟩, .output 0]
+            #[⟨0, 1⟩, ⟨1, 2⟩]
+-- TODO test panicking
+-- #lspec test "trying to identity remove spider with phase should fail" ((aZSpiderWithPhase.identityRemoval 1).get! == justPhaseFreeXSpiderIdentityRemoved)
+
 end LSpec
