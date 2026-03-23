0bd584f86a9f0769a2927c1ef387272a9b5c2514
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-03-23 11:16:10 +0100

    Get Pyodide running inside the InfoView

---
+++ zx_view_widget/src/pyodide-bundled.d.ts
@@ -0,0 +1,16 @@
+declare module 'pyodide-bundled/asm-js' {
+  const dataUrl: string
+  export default dataUrl
+}
+declare module 'pyodide-bundled/wasm' {
+  const dataUrl: string
+  export default dataUrl
+}
+declare module 'pyodide-bundled/stdlib' {
+  const dataUrl: string
+  export default dataUrl
+}
+declare module 'pyodide-bundled/lock' {
+  const contents: unknown
+  export default contents
+}
