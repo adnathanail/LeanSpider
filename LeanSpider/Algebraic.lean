2483f7ab3d134ac15682680edf4bb79fdc3ea563
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-05-10 18:54:18 +0200

    Neaten spider fusion proof

--- LeanSpider/Algebraic.lean
+++ LeanSpider/Algebraic.lean
@@ -1,6 +1,7 @@
 import LeanSpider.Algebraic.Phase
 import LeanSpider.Algebraic.ZX
 import LeanSpider.Algebraic.Semantics
+import LeanSpider.Algebraic.Congruence
 import LeanSpider.Algebraic.SpiderFusion
 import LeanSpider.Algebraic.Visualize
 import LeanSpider.Algebraic.Gates
